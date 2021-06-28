37682bd45895e9749543c4e30a82725ba1be070a
Hrishikesh Terdalkar <hrishirt@cse.iitk.ac.in>
2021-06-29 02:27:17 +0530

    Bump version: 0.1.3 → 0.2.0

--- google_drive_ocr/__init__.py
+++ google_drive_ocr/__init__.py
@@ -4,6 +4,6 @@ from .application import GoogleOCRApplication
 
 __author__ = """Hrishikesh Terdalkar"""
 __email__ = 'hrishikeshrt@linuxmail.org'
-__version__ = '0.1.3'
+__version__ = '0.2.0'
 
 __all__ = [GoogleOCRApplication]
