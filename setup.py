60eb4c9e2ed945e6dfbd1afe8d4150d761431e47
Hrishikesh Terdalkar <hrishirt@cse.iitk.ac.in>
2022-02-10 02:56:10 +0530

    "build(release): bump version 0.2.3 → 0.2.4"

--- setup.py
+++ setup.py
@@ -52,6 +52,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/hrishikeshrt/google_drive_ocr',
-    version='0.2.3',
+    version='0.2.4',
     zip_safe=False,
 )
